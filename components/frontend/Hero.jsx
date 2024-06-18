fe2473a6fc0cebfb55118e1bfd08c573395b4e29
Ujjwal <ujjwaljindal835@gmail.com>
2024-06-18 16:02:33 +0530

    Fixed the Hero Carousal by using Swipper and also added a advertisement in rightmost hero componnet

--- components/frontend/Hero.jsx
+++ components/frontend/Hero.jsx
@@ -3,16 +3,22 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import HeroCarousel from "./HeroCarousel";
+import { CircleDollarSign, FolderSync, HelpCircle } from "lucide-react";
+import sale from "../../public/sale.gif";
 
 export default function Hero() {
-  const categories = [{}, {}, {}, {}, {}, {}, {}];
+  const categories = [{},{} , {}, {}, {},{},{} , {}, {}, {}];
   return (
-    <div className="flex gap-8 mb-6">
-      <div className="w-1/3 bg-white border border-gray-300 rounded-lg dark:bg-gray-700 dark:border-gray-700 text-slate-800 overflow-hidden ">
+    <div className="grid grid-cols-12 gap-8 mb-6">
+      {/* categories */}
+      <div className="hidden sm:block sm:col-span-3 bg-white border border-gray-300 rounded-lg dark:bg-gray-700 dark:border-gray-700 text-slate-800 overflow-hidden">
         <h2 className="bg-slate-100 dark:bg-gray-800 border-b border-gray-300 dark:border-gray-600 py-3 px-6 font-semibold text-slate-800 dark:text-slate-100  ">
           Shop By Category
         </h2>
-        <div className="py-3 px-6 h-auto overflow-y-auto flex flex-col gap-2 ">
+        <div
+          className="py-3 px-6 overflow-y-auto flex flex-col gap-2 "
+          style={{ maxHeight: "400px" }}
+        >
           {categories.map((category, i) => {
             return (
               <Link
@@ -25,7 +31,7 @@ export default function Hero() {
                   width={556}
                   height={556}
                   alt="Vegetables"
-                  className="w-12 h-12 rounded-full object-cover border border-lime-300"
+                  className="w-10 h-10 rounded-full object-cover border border-lime-300"
                 />
                 <span className="text-sm">Vegetables</span>
               </Link>
@@ -33,8 +39,37 @@ export default function Hero() {
           })}
         </div>
       </div>
-      <div className="w-2/3 bg-blue-500 rounded-md">
+      {/* banners */}
+      <div className="col-span-full sm:col-span-7 rounded-md">
         <HeroCarousel />
+      </div>
+      {/* help and sale advertisement */}
+      <div className="col-span-2 hidden sm:block bg-white p-3 dark:bg-slate-800 rounded-lg">
+        <Link href="#" className="flex items-center space-x-1 mb-3">
+          <HelpCircle className="shrink-0 w-5 h-5 dark:text-lime-500 text-slate-900" />
+          <div className="flex flex-col">
+            <h2 className="uppercase text-sm">Help Center</h2>
+            <p className="text-[0.7rem]">Guide To Customer Care</p>
+          </div>
+        </Link>
+        <Link href="#" className="flex items-center space-x-1 mb-3">
+          <FolderSync className="shrink-0 w-5 h-5 dark:text-lime-500 text-slate-900" />
+          <div className="flex flex-col">
+            <h2 className="uppercase text-sm">Easy Return</h2>
+            <p className="text-[0.7rem]">Quick Return</p>
+          </div>
+        </Link>
+        <Link
+          href="/register-farmer"
+          className="flex items-center space-x-1 mb-6 "
+        >
+          <CircleDollarSign className="shrink-0 w-5 h-5 dark:text-lime-500 text-slate-900" />
+          <div className="flex flex-col">
+            <h2 className="uppercase text-sm">Sell on JindalShop</h2>
+            <p className="text-[0.7rem]">Million of Visitors</p>
+          </div>
+        </Link>
+        <Image src={sale} alt="Sale" className="w-full rounded-lg" />
       </div>
     </div>
   );
--- components/frontend/HeroCarousel.jsx
+++ components/frontend/HeroCarousel.jsx
@@ -1,27 +1,91 @@
 "use client";
 import Image from "next/image";
 import Link from "next/link";
-import { Carousel } from "nuka-carousel";
-import React from "react";
+import React, { useState } from "react";
+import { Pagination } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import BannerSliderBtns from "./BannerSliderBtns";
 
+const banners = [
+  {
+    title: "Vegetables",
+    image: "/banners/vegetables.jpg",
+  },
+  {
+    title: "Breads",
+    image: "/banners/breads.jpg",
+  },
+  {
+    title: "Dairy",
+    image: "/banners/dairy.jpg",
+  },
+  {
+    title: "Drinks",
+    image: "/banners/drinks.jpg",
+  },
+  {
+    title: "Fruits",
+    image: "/banners/fruits.jpg",
+  },
+  {
+    title: "Grocery",
+    image: "/banners/grocery.jpg",
+  },
+  {
+    title: "Spices",
+    image: "/banners/spices.jpg",
+  },
+  {
+    title: "Pulses",
+    image: "/banners/pulses.jpg",
+  },
+];
 export default function HeroCarousel() {
+  const [banner, setBanner] = useState(banners[0]);
+
+  const handleSlideChange = (swiper) => {
+    // get current slide index
+    const currentIndex = swiper.activeIndex;
+    // update project state based on current slide index
+    setBanner(banners[currentIndex]);
+  };
   return (
-    <Carousel
-      wrapMode="wrap"
-      autoplay
-      showArrows
-      className="rounded-md overflow-hidden object-cover"
+    <Swiper
+      spaceBetween={30}
+      modules={[Pagination]}
+      slidesPerView={1}
+      className=""
+      onSlideChange={handleSlideChange}
+      pagination={{ clickable: true }}
     >
-      <Link href="#" className="">
-        <Image width={712} height={384} src="/banners/fruits.jpg" className="w-full" />
-      </Link>
-      <img src="/banners/vegetables.jpg" className="w-full" />
-      <img src="/banners/breads.jpg" className="w-full" />
-      <img src="/banners/dairy.jpg" className="w-full" />
-      <img src="/banners/drinks.jpg" className="w-full" />
-      <img src="/banners/grocery.jpg" className="w-full" />
-      <img src="/banners/pulses.jpg" className="w-full" />
-      <img src="/banners/spices.jpg" className="w-full" />
-    </Carousel>
+      {banners.map((banner, index) => {
+        return (
+          <SwiperSlide key={index} className="w-full">
+            <div className="h-[490px] relative group flex justify-center items-center bg-pink-50/20 rounded-md">
+              {/* image */}
+              <div className="relative w-full h-full">
+                <Link href="/dashboard">
+                  <Image
+                    src={banner.image}
+                    fill
+                    className="object-cover w-full"
+                    alt={banner.title}
+                  />
+                </Link>
+              </div>
+            </div>
+          </SwiperSlide>
+        );
+      })}
+
+      {/* slider button */}
+      <BannerSliderBtns
+        containerStyles="flex gap-2 absolute right-0 bottom-[calc(50%_-_22px)] z-20 w-full justify-between  xl:justify-between"
+        btnStyles="bg-slate-800 hover:bg-slate-500 text-lime-500 text-[22px] w-[44px] h-[44px] flex justify-center items-center transition-all rounded-full "
+        iconsStyles="text-4xl text-slate-700 dark:text-slate-50 transition-colors duration-300 hover:text-lime-500 dark:hover:text-lime-500"
+      />
+    </Swiper>
   );
 }
